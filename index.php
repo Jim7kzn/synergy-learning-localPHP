14bdb481bd0b0635c10e590d9d46165ac80a5f54
Jim7rfp <Jim7fp@gmail.com>
2022-09-05 12:42:58 +0300

    02. Д/З - Начинаем погружение, Подробнее о переменных

--- index.php
+++ index.php
@@ -1,11 +1,38 @@
 <?php
-	echo "И снова - здравствуй, мир!";
-	echo "<br>";
-	echo "И снова - <b>здравствуй</b>, мир!";
-	echo "<br>\"Это цитата\"";
+	$jobNum = 0;
+	$lf = "<br>";
+	$separator = "{$lf}<hr>";
+	$prefix = "<b><i>";
+	$suffix = "</i></b>{$lf}";
 
-	$a = 9;
-	$b = .86;
-	$c = 'str';
-	echo "<br> {$a} {$b} {$c}";
+//1
+	$jobNum ++;
+	echo "{$separator}{$prefix}Задание&nbsp;{$jobNum}.{$suffix}";
+
+	$hello = "Hello";
+	$name = "Anna";
+
+	echo "{$hello}, {$name}!{$lf}";
+
+//2
+	$jobNum ++;
+	echo "{$separator}{$prefix}Задание&nbsp;{$jobNum}.{$suffix}";
+
+	$str1 = '<i>"Аптеку позабудь ты для венков лавровых';
+	$str2 = "И не мори больных, но усыпляй здоровых.\"</i>";
+
+	echo "{$str1}{$lf}";
+	echo "{$str2}{$lf}";
+
+//3
+	$jobNum ++;
+	echo "{$separator}{$prefix}Задание&nbsp;{$jobNum}.{$suffix}";
+
+	$secondsPerMinute = 60;
+	$minutesPerHour = 60;
+	$howSeconds = $secondsPerMinute * $minutesPerHour;
+
+	echo "В часе {$howSeconds} секунд.{$lf}";
+
+
 
