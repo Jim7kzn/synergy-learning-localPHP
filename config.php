26a2a326bbb1df031caf706fb3c41e9729db1505
Jim7rfp <Jim7fp@gmail.com>
2022-09-07 14:28:58 +0300

    12. Д/З - Как "писать" сайты

--- config.php
+++ config.php
@@ -1,11 +1,20 @@
 <?php
+	$lf = "<br>";
+//	$separator = "{$lf}<hr>";
+//	$prefix = "<b><i>";
+//	$suffix = "</i></b>{$lf}";
+//	$redStr = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+
 	function myDebug($arr, $die = false){
 		echo '<pre>' . print_r($arr, true) . '</pre>';
 		if ($die) die;
 	}
 
-	$lf = "<br>";
-	$separator = "{$lf}<hr>";
-	$prefix = "<b><i>";
-	$suffix = "</i></b>{$lf}";
-	$redStr = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+	function newHomeWork($numJob, $fl = false) {
+		$lf = "<br>";
+		$separator = (!$fl ? $lf . $lf : '') . "<hr>";
+		$prefix = "<b><i>";
+		$suffix = "</i></b>{$lf}";
+		echo "{$separator}{$prefix}Задание&nbsp;{$numJob}.{$suffix}";
+	}
+
--- lesson10.php
+++ lesson10.php
@@ -1,20 +1,17 @@
 <?php
-	require_once 'config.php';    // д/з 10
+	require_once 'config.php';    				// д/з 10
 	$jobNum = 0;
 
-//1
-	echo "{$separator}{$prefix}Задание&nbsp;" . (++$jobNum) . ".{$suffix}";
+	newHomeWork(++$jobNum, true);		// задание 1
 	$dateNew = "2023-03-08";
 	$sec = strtotime($dateNew) - strtotime(date("Y-M-d"));
 	echo intval($sec / 3600 / 24);
 
-//2
-	echo "{$separator}{$prefix}Задание&nbsp;" . (++$jobNum) . ".{$suffix}";
+	newHomeWork(++$jobNum);				// задание 2
 	$dateNew = "2022-02-23";
 	echo date("d-m-Y", strtotime($dateNew));
 
-//3
-	echo "{$separator}{$prefix}Задание&nbsp;" . (++$jobNum) . ".{$suffix}";
+	newHomeWork(++$jobNum);				// задание 3
 	$dateNew = date("Y", time()) . "-01-01";
 	$sec = strtotime(date("Y-M-d")) - strtotime($dateNew);
 	echo intval($sec / 3600 / 24) + 1;
